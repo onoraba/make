e4d05fa4524010fb5e8f8afef0388f918c314d30
user <user@void>
2023-10-25 18:46:19 +0300

    clean

--- init/etc/cfg.sh
+++ init/etc/cfg.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-br=out
+br=out # bridge name
 uplink_dev=$(first_ether) # or manual
 test -z "${uplink_dev}" && exit 1
-br_self_vlan=202
-vlans=212,215,2-5
+br_self_vlan=202 # bridge self vlan id
+vlans=212,215,2-5 # comma separated, could be ranges
